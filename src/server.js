a86b86472651fe27712a884f3bcd5af5502f778d
aNdr3W03 <andrewbjamesie@yahoo.com>
2023-01-23 13:48:14 +0700

    update server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server run at ${server.info.uri}`);
+};
+
+init();
